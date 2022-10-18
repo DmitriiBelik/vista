fcc172677e195f4ede530ae79a1d1dda627389c3
Dmitrii Belik <dima.belik.011100@yandex.ru>
2022-10-18 11:22:16 +0300

    fix prettier

--- src/components/basicTabs.js
+++ src/components/basicTabs.js
@@ -1,16 +1,18 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import {Tabs, Tab, Typography, Box} from "@mui/material/";
-import { useSelector, useDispatch } from "react-redux";
-import {patientInfoFetched} from '../redux/MainSlice'
+import { Tabs, Tab, Typography, Box } from '@mui/material/'
+import PropTypes from 'prop-types'
+import * as React from 'react'
 import { useState } from 'react'
-import BasicTable from "./table";
+import { useSelector, useDispatch } from 'react-redux'
+
+import BasicTable from './table'
+
+import { patientInfoFetched } from '../redux/MainSlice'
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props
     return (
         <div
-            role="tabpanel"
+            role='tabpanel'
             hidden={value !== index}
             id={`simple-tabpanel-${index}`}
             aria-labelledby={`simple-tab-${index}`}
@@ -22,49 +24,45 @@ function TabPanel(props) {
                 </Box>
             )}
         </div>
-  );
+    )
 }
 
 TabPanel.propTypes = {
     children: PropTypes.node,
     index: PropTypes.number.isRequired,
     value: PropTypes.number.isRequired,
-};
+}
 
 function a11yProps(index) {
     return {
         id: `simple-tab-${index}`,
-        "aria-controls": `simple-tabpanel-${index}`,
-    };
+        'aria-controls': `simple-tabpanel-${index}`,
+    }
 }
 
 export default function BasicTabs() {
-    const dispatch = useDispatch();
-    const {presentList, quittingList} = useSelector(state => state.main)
-    const [value, setValue] = useState(0);
+    const dispatch = useDispatch()
+    const { presentList, quittingList } = useSelector((state) => state.main)
+    const [value, setValue] = useState(0)
     const handleChange = (event, newValue) => {
-        setValue(newValue);
+        setValue(newValue)
         dispatch(patientInfoFetched({}))
-    };
+    }
 
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label={`Присутствуют(${presentList.length})`} {...a11yProps(0)} />
-          <Tab label={`Выбывшие(${quittingList.length})`} {...a11yProps(1)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        <BasicTable status='presentList' data={presentList}/>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-      <BasicTable status='quittingList' data={quittingList}/>
-      </TabPanel>
-    </Box>
-  );
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} aria-label='basic tabs example'>
+                    <Tab label={`Присутствуют(${presentList.length})`} {...a11yProps(0)} />
+                    <Tab label={`Выбывшие(${quittingList.length})`} {...a11yProps(1)} />
+                </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+                <BasicTable status='presentList' data={presentList} />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <BasicTable status='quittingList' data={quittingList} />
+            </TabPanel>
+        </Box>
+    )
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux';
-import Store from './redux/Store';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { Provider } from 'react-redux'
+
+import App from './App'
+import Store from './redux/Store'
 import './index.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <Provider store={Store}>
         <App />
-    </Provider>
-);
-
+    </Provider>,
+)
